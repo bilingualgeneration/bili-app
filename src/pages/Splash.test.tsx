f5a41e56dcd32cfaf31db52aa8ef345194f14231
Aleksandra Makhrova <amakhro@gmail.com>
2023-09-19 23:13:30 -0700

    Tests were added for Splash page

---
+++ src/pages/Splash.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Splash from './Splash';
+
+describe('<Splash />', () => {
+  it('should render the title', () => {
+    render(<Splash />);
+    const titleElement = screen.getByText('Welcome to Bili');
+    expect(titleElement).toBeVisible();
+  });
+
+  it('should render the Sign Up button', () => {
+    render(<Splash />);
+    const signUpButton = screen.getByText('Sign Up');
+    expect(signUpButton).toBeVisible();
+  });
+
+
+  // Uncomment the following test when the Sign In button enabled:
+  // it('should render the Sign In button', () => {
+  //   render(<Splash />);
+  //   const signInButton = screen.getByText('Sign In');
+  //   expect(signInButton).toBeVisible();
+  // });
+});
