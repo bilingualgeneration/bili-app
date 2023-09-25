4d586dfd1ccea1f2eaab8551714d07c091f7adc8
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-09-25 17:26:51 -0500

    Added functionality to StudentDashboard page.

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -7,10 +7,12 @@ import {
   IonSelect,
   IonSelectOption,
 } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
 
 const StudentDashboard: React.FC = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState<string>('');
+  const history = useHistory();
 
   const handleStartClick = () => {
     setDropdownOpen(!isDropdownOpen);
@@ -20,26 +22,58 @@ const StudentDashboard: React.FC = () => {
     setSelectedOption(event.detail.value);
   };
 
+  const navigateToRoute = (route: string) => {
+    history.push(route);
+    setDropdownOpen(false);
+  };
+
   return (
     <>
-        <IonButton expand="block" onClick={handleStartClick}>
-            Start
-        </IonButton>
-        {isDropdownOpen && (
-            <IonList>
-            <IonItem>
-                <IonLabel>Select an Option:</IonLabel>
-                <IonSelect
-                value={selectedOption}
-                onIonChange={handleOptionChange}
+      <IonButton expand="block" onClick={handleStartClick}>
+        Start
+      </IonButton>
+      {isDropdownOpen && (
+        <IonList>
+          <IonItem>
+            <IonLabel>Select an Option:</IonLabel>
+            <IonSelect
+              value={selectedOption}
+              onIonChange={handleOptionChange}
+            >
+              <IonSelectOption value="journeys">Journeys</IonSelectOption>
+              <IonSelectOption value="explore">Explore</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          {selectedOption === 'explore' && (
+            <>
+              <IonItem>
+                <IonButton
+                  expand="block"
+                  onClick={() => navigateToRoute('/stories-carousel')}
                 >
-                <IonSelectOption value="journeys">Journeys</IonSelectOption>
-                <IonSelectOption value="explore">Explore</IonSelectOption>
-                </IonSelect>
-            </IonItem>
-            </IonList>
-        )}
-      
+                  Stories Carousel
+                </IonButton>
+              </IonItem>
+              <IonItem>
+                <IonButton
+                  expand="block"
+                  onClick={() => navigateToRoute('/wellness-carousel')}
+                >
+                  Wellness Carousel
+                </IonButton>
+              </IonItem>
+              <IonItem>
+                <IonButton
+                  expand="block"
+                  onClick={() => navigateToRoute('/play-carousel')}
+                >
+                  Play Carousel
+                </IonButton>
+              </IonItem>
+            </>
+          )}
+        </IonList>
+      )}
     </>
   );
 };
