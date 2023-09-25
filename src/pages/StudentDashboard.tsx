e63434d9975be237e9cbb2a60b30c9ba509be859
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-09-25 17:02:33 -0500

    Corrected StudentDashboard page and page now gives user option to choose between journeys and explore.

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -1,8 +1,47 @@
-import { IonPage } from "@ionic/react";
+import React, { useState } from 'react';
+import {
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/react';
 
 const StudentDashboard: React.FC = () => {
-    return <IonPage>student dashboard page</IonPage>
-    
-}
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
+  const handleStartClick = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleOptionChange = (event: CustomEvent) => {
+    setSelectedOption(event.detail.value);
+  };
+
+  return (
+    <>
+        <IonButton expand="block" onClick={handleStartClick}>
+            Start
+        </IonButton>
+        {isDropdownOpen && (
+            <IonList>
+            <IonItem>
+                <IonLabel>Select an Option:</IonLabel>
+                <IonSelect
+                value={selectedOption}
+                onIonChange={handleOptionChange}
+                >
+                <IonSelectOption value="journeys">Journeys</IonSelectOption>
+                <IonSelectOption value="explore">Explore</IonSelectOption>
+                </IonSelect>
+            </IonItem>
+            </IonList>
+        )}
+      
+    </>
+  );
+};
 
 export default StudentDashboard;
