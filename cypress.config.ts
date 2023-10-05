3a092a5154329def3594f4c6c09ad5bebcc6aa1a
Jon Chin <jon@sharemeals.org>
2023-10-05 08:46:54 -0400

    updated testing port

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8100",
+    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
