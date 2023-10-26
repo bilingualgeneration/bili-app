f115d0d778eaf5cede6741c4cf1c62959f3f7906
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-10-26 10:42:38 -0500

    Edited caching mechanism to store translation for dif locales to improve loading times.

--- i18n.ts
+++ i18n.ts
@@ -4,21 +4,34 @@ import { createIntl, createIntlCache, IntlShape } from 'react-intl';
 const locales: string[] = ['en', 'es', 'es-inc'];
 
 // Set the default locale
-const defaultLocale: string = 'en';
+const defaultLocale: string = 'es';
+
+// Create a cache object for translations
+const translationsCache: { [locale: string]: Promise<Record<string, string>> } = {};
 
 // Function to read JSON translations for a specific locale
 async function readTranslations(locale: string) {
-  try {
-    return await import(`./lang/${locale}.json`);
-  } catch (error) {
-    console.error(`Translations for ${locale} not found. ${error}`);
+  if (await translationsCache[locale]) {
+    return translationsCache[locale];
   }
+  
+  try {
+    const translations = await import(`./lang/${locale}.json`);
+    translationsCache[locale] = translations;
+    return translations;
+  } catch (error) {
+    console.error(`Translations for ${locale} not found.`);
+  }
+  
   return {};
 }
 
 // Create a cache and an intl object
 const cache = createIntlCache();
-const intl: IntlShape = createIntl({ locale: defaultLocale, messages: await readTranslations(defaultLocale) }, cache);
+const intl: IntlShape = createIntl(
+  { locale: defaultLocale, messages: await readTranslations(defaultLocale) },
+   cache
+);
 
 export { locales, defaultLocale, intl };
 
