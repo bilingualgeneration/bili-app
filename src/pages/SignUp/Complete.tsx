111ea05daae2683b2e22eaa045bd0d75916f4792
Aleksandra Makhrova <amakhro@gmail.com>
2023-10-29 22:27:22 -0700

    Updated Complete.tsx and moved happy-cactus.png to public/assets

--- src/pages/SignUp/Complete.tsx
+++ src/pages/SignUp/Complete.tsx
@@ -1,8 +1,30 @@
+import { IonButton, IonImg } from "@ionic/react"
 
 export const Complete: React.FC = () => {
     return (
 	<>
-	    complete!
+	    <div 
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+			}}
+		>
+			<h1>Success! You did it!</h1>
+			<IonImg
+				src="/assets/img/happy_cactus.png"
+    		>
+			</IonImg>
+			<IonButton
+				shape='round'
+				type='submit'
+				data-testid='complete-continue-button'
+			>
+			Continue
+			</IonButton>
+		</div>
+
 	</>
+	
+
     );
 }
