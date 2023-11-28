8454bb1b15121d557962ea662e99f91d4288bd5c
Jon Chin <jon@sharebyteaching.org>
2023-11-28 01:59:49 -0500

    fixed firebase function issue

--- packages/server/functions/src/user/signup.tsx
+++ packages/server/functions/src/user/signup.tsx
@@ -1,12 +1,12 @@
 const admin = require("firebase-admin");
-import { onRequest } from "firebase-functions/v2/https";
-//import * as logger from "firebase-functions/logger";
+import { onCall } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
-export const signup = onRequest(async (request, response) => {
+export const signup = onCall(async (request) => {
   // todo: validate input
   // todo: check if errors in sign up, such as if email address already in use
-  console.log(request.body.data);
-  const data = request.body.data;
+  const data = request.data;
+  logger.log(data);
   const userRecord = await admin.auth().createUser({
     email: data.email,
     password: data.password,
@@ -23,5 +23,4 @@ export const signup = onRequest(async (request, response) => {
     school: data.school,
     schoolRoles: data.schoolRoles,
   });
-  response.send({ data: {} });
 });
