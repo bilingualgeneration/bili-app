2c365848faa0d5827a33206be602c1354e6c39fe
Jon Chin <jon@sharebyteaching.org>
2024-01-23 03:07:48 -0500

    updated

--- packages/app/src/pages/Settings/Profile.tsx
+++ packages/app/src/pages/Settings/Profile.tsx
@@ -1,37 +1,48 @@
-import {
-  addOutline,
-  chevronForwardCircle,
-  chevronForwardCircleOutline,
-  ellipse,
-  ellipsisHorizontal,
-  sparkles,
-} from "ionicons/icons";
-import { FormattedMessage } from "react-intl";
-import {
-  IonButton,
-  IonButtons,
-  IonPage,
-  IonHeader,
-  IonTitle,
-  IonList,
-  IonItem,
-  IonLabel,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonIcon,
-  IonCard,
-  IonInput,
-  IonSelect,
-  IonSelectOption,
-} from "@ionic/react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { httpsCallable } from "firebase/functions";
+import { IonButton, IonGrid, IonRow, IonCol } from "@ionic/react";
+import { Input } from "@/components/Input";
 import { useMaskito } from "@maskito/react";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useFunctions } from "reactfire";
 import { useProfile } from "@/contexts/ProfileContext";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import "./Profile.css";
 
 export const Profile: React.FC = () => {
+  const intl = useIntl();
+  const schema = z.object({
+    // todo: better schema
+    name: z.string().nullable(),
+    phone: z.string().nullable(),
+    email: z.string().email().nullable(),
+    dob: z.string().nullable(),
+    country: z.string().nullable(),
+  });
+  const { name, phone, email, dob, country } = useProfile();
+  const { control, handleSubmit } = useForm<z.infer<typeof schema>>({
+    defaultValues: {
+      country,
+      dob,
+      email,
+      name,
+      phone,
+    },
+    mode: "onBlur",
+    resolver: zodResolver(schema),
+  });
+  const functions = useFunctions();
+  const updateParentProfileFunction = httpsCallable(
+    functions,
+    "user-parent-profile-update",
+  );
+
+  const onSubmit = handleSubmit((response) => {
+    updateParentProfileFunction(response);
+  });
   const phoneMask = useMaskito({
     options: {
       mask: [
@@ -66,140 +77,116 @@ export const Profile: React.FC = () => {
 
   return (
     <>
-      <div className="settings-pg1-container">
-        <IonGrid>
-          <IonRow className="ion-justify-content-between row">
-            <IonCol size="auto">
-              <h1 className="child-profile-heading">
-                <FormattedMessage
-                  id="settings.adult"
-                  defaultMessage="Adult Profile"
-                  description="Adult Profile page title in settings"
-                />
-              </h1>
-            </IonCol>
-          </IonRow>
-
-          <div className="adult-profile-content">
-            <IonRow>
-              <IonCol>
-                <IonLabel>
+      <form onSubmit={onSubmit}>
+        <div className="settings-pg1-container">
+          <IonGrid>
+            <IonRow className="ion-justify-content-between row">
+              <IonCol size="auto">
+                <h1 className="child-profile-heading">
                   <FormattedMessage
-                    id="settings.fullName"
-                    defaultMessage="Full name"
-                    description="Full name label WITHOUT required asterix in settings"
+                    id="settings.adult"
+                    defaultMessage="Adult Profile"
+                    description="Adult Profile page title in settings"
                   />
-                </IonLabel>
-                <IonInput
-                  className="input"
-                  shape="round"
-                  fill="outline"
-                  placeholder="Bilingual Generation"
-                ></IonInput>
+                </h1>
               </IonCol>
             </IonRow>
 
-            <IonRow>
-              <IonCol>
-                <IonLabel>
-                  <FormattedMessage
-                    id="settings.phone"
-                    defaultMessage="Phone number"
-                    description="Phone number label in settings"
-                  />
-                </IonLabel>
-                <IonInput
-                  ref={async (phoneInput) => {
-                    if (phoneInput) {
-                      const input = await phoneInput.getInputElement();
-                      phoneMask(input);
-                    }
-                  }}
-                  className="input"
-                  type="tel"
-                  shape="round"
-                  fill="outline"
-                  placeholder="+1 (xxx) xxx-xxxx"
-                ></IonInput>
-              </IonCol>
-            </IonRow>
+            <div className="adult-profile-content">
+              <Input
+                label={intl.formatMessage({
+                  id: "settings.fullName",
+                  defaultMessage: "Full name",
+                  description:
+                    "Full name label WITHOUT required asterix in settings",
+                })}
+                labelPlacement="above"
+                name="name"
+                fill="outline"
+                control={control}
+                type="text"
+              />
 
-            <IonRow>
-              <IonCol>
-                <IonLabel>
-                  <FormattedMessage
-                    id="settings.email"
-                    defaultMessage="Email address"
-                    description="Email address label WITHOUT required asterix in settings"
-                  />
-                </IonLabel>
-                <IonInput
-                  className="input"
-                  type="email"
-                  shape="round"
-                  fill="outline"
-                  placeholder="bilingual.generation@bili.com"
-                ></IonInput>
-              </IonCol>
-            </IonRow>
+              <br />
 
-            <IonRow>
-              <IonCol>
-                <IonLabel>
-                  <FormattedMessage
-                    id="settings.dob"
-                    defaultMessage="Date of birth"
-                    description="Date of birth input label in settings"
-                  />
-                </IonLabel>
-                <IonInput
-                  className="input date-type"
-                  type="date"
-                  shape="round"
-                  fill="outline"
-                ></IonInput>
-              </IonCol>
-            </IonRow>
+              <Input
+                label={intl.formatMessage({
+                  id: "settings.phone",
+                  defaultMessage: "Phone number",
+                  description: "Phone number label in settings",
+                })}
+                labelPlacement="above"
+                name="phone"
+                fill="outline"
+                control={control}
+                type="text"
+              />
 
-            <IonRow>
-              <IonCol>
-                <IonLabel>
-                  <FormattedMessage
-                    id="settings.country"
-                    defaultMessage="Country"
-                    description="Country input label in settings"
-                  />
-                </IonLabel>
-                <IonSelect
-                  className="country-dropdown"
-                  interface="popover"
-                  value={selectedCountry}
-                  onIonChange={handleCountryChange}
-                  justify="space-between"
-                  label={selectedCountry ? undefined : "Select one"}
-                  label-placement="floating"
-                >
-                  <IonSelectOption className="country-dropdown-options">
-                    United States
-                  </IonSelectOption>
-                </IonSelect>
-              </IonCol>
-            </IonRow>
+              <br />
 
-            <IonRow>
-              <IonCol class="col-save-changes-button">
-                <IonButton shape="round" className="save-changes-button">
-                  <FormattedMessage
-                    id="settings.saveChangeBtn"
-                    defaultMessage="Save changes"
-                    description="Save changes button label for Adult Profile within settings page"
-                  />
-                </IonButton>
-              </IonCol>
-            </IonRow>
-          </div>
-        </IonGrid>
-      </div>
+              <Input
+                label={intl.formatMessage({
+                  id: "settings.email",
+                  defaultMessage: "Email address",
+                  description:
+                    "Email address label WITHOUT required asterix in settings",
+                })}
+                labelPlacement="above"
+                name="email"
+                fill="outline"
+                control={control}
+                type="text"
+              />
+
+              <br />
+
+              <Input
+                label={intl.formatMessage({
+                  id: "settings.dob",
+                  defaultMessage: "Date of birth",
+                  description: "Date of birth input label in settings",
+                })}
+                labelPlacement="above"
+                name="dob"
+                fill="outline"
+                control={control}
+                type="date"
+              />
+
+              <br />
+
+              <Input
+                label={intl.formatMessage({
+                  id: "settings.country",
+                  defaultMessage: "Country",
+                  description: "Country input label in settings",
+                })}
+                labelPlacement="above"
+                name="country"
+                fill="outline"
+                control={control}
+                type="text"
+              />
+
+              <IonRow>
+                <IonCol class="col-save-changes-button">
+                  <IonButton
+                    shape="round"
+                    className="save-changes-button"
+                    type="submit"
+                  >
+                    <FormattedMessage
+                      id="settings.saveChangeBtn"
+                      defaultMessage="Save changes"
+                      description="Save changes button label for Adult Profile within settings page"
+                    />
+                  </IonButton>
+                </IonCol>
+              </IonRow>
+            </div>
+          </IonGrid>
+        </div>
+      </form>
     </>
   );
 };
