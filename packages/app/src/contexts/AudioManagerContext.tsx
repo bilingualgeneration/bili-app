ab872ef6e30d5776b3e1c7748b22374772ec698d
Jon Chin <jon@sharebyteaching.org>
2024-02-06 19:08:49 -0500

    added

---
+++ packages/app/src/contexts/AudioManagerContext.tsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export interface AudioManager {
+  addAudio: any;
+  callback: any;
+  setCallback: any;
+  clearAudio: any;
+}
+
+const defaultState: AudioManager = {
+  addAudio: () => {},
+  callback: () => {},
+  clearAudio: () => {},
+  setCallback: () => {},
+};
+
+const AudioManagerContext = createContext<AudioManager>(defaultState);
+
+export const useAudioManager = () => useContext(AudioManagerContext);
+
+export const AudioManagerProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [audios, setAudios] = useState<any[]>([]);
+  const [callback, setCallback] = useState<any>(() => {});
+  useEffect(() => {
+    if (audios.length > 0) {
+      audios[0].onended = () => {
+        if (audios.length === 1) {
+          // last one played
+          callback();
+        }
+        setAudios(audios.slice(1));
+      };
+      audios[0].play();
+    }
+  }, [audios]);
+  const addAudio = (inputAudios: any[]) => {
+    // silence existing audio
+    // todo: probably not working
+    audios.forEach((a) => {
+      a.pause();
+    });
+    setAudios(inputAudios.map((a) => new Audio(a)));
+  };
+
+  const clearAudio = () => {
+    // todo: not working
+    audios.forEach((a) => {
+      a.pause();
+    });
+    setAudios([]);
+  };
+  return (
+    <AudioManagerContext.Provider
+      value={{
+        addAudio,
+        callback,
+        setCallback,
+        clearAudio,
+      }}
+    >
+      {children}
+    </AudioManagerContext.Provider>
+  );
+};
