1785d1f280730fc9506ee24fe570296d0971d624
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2024-02-07 01:05:23 -0500

    first draft of intro component

---
+++ packages/app/src/pages/Intro/Intro.tsx
@@ -0,0 +1,135 @@
+import React, { FC, useEffect } from "react";
+import {
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonGrid,
+  IonImg,
+  IonRow,
+  IonText,
+} from "@ionic/react";
+import { FormattedMessage } from "react-intl";
+import { useProfile } from "@/contexts/ProfileContext";
+import { useHistory } from "react-router-dom";
+import audio_en_file from "@/assets/audio/story_factory_first_en.mp3";
+import audio_es_file from "@/assets/audio/story_factory_first_es.mp3";
+import audio_es_inc_file from "@/assets/audio/story_factory_first_es-inc.mp3";
+import "./Intro.scss";
+
+/*
+an array of objects
+each object needs en, es, es-inc (both text and audio
+in addition to the array (not part of the array), an image
+also a router path to go next
+*/
+
+interface Type {
+  text: string;
+  audio: string;
+}
+interface IntroProps {
+  en: Type;
+  es: Type;
+  esInc: Type;
+}
+
+export const Intro = (data: IntroProps[], image: string, nextPath: string) => {
+  const { isInclusive, isImmersive } = useProfile();
+  const history = useHistory();
+  const audio_en = new Audio(audio_en_file);
+  const audio_es = new Audio(audio_es_file);
+  const audio_es_inc = new Audio(audio_es_inc_file);
+  useEffect(() => {
+    return () => {
+      audio_en.pause();
+      audio_es.pause();
+      audio_es_inc.pause();
+    };
+  });
+  useEffect(() => {
+    if (isImmersive) {
+      if (isInclusive) {
+        audio_es_inc.onended = () => {
+          history.push(nextPath);
+        };
+        audio_es_inc.play();
+      } else {
+        audio_es.onended = () => {
+          history.push(nextPath);
+        };
+        audio_es.play();
+      }
+    } else {
+      audio_en.onended = () => {
+        history.push(nextPath);
+      };
+      if (isInclusive) {
+        audio_es_inc.onended = () => {
+          audio_en.play();
+        };
+        audio_es_inc.play();
+      } else {
+        audio_es.onended = () => {
+          audio_en.play();
+        };
+        audio_es.play();
+      }
+    }
+  }, []);
+  return (
+    <div style={{ position: "relative" }}>
+      <div className="ion-no-padding sf-card">
+        <IonCard className="ion-no-margin">
+          <IonCardContent>
+            <div style={{ paddingRight: 100 }}>
+              <h1 className="color-selva">
+                {isInclusive && (
+                  <FormattedMessage
+                    id="storyFactory.welcome_inc"
+                    defaultMessage="Welcome to the Story Factory!"
+                    description="Main welcome message on Story Factory"
+                  />
+                )}
+                {!isInclusive && (
+                  <FormattedMessage
+                    id="storyFactory.welcome"
+                    defaultMessage="Welcome to the Story Factory!"
+                    description="Main welcome message on Story Factory"
+                  />
+                )}
+              </h1>
+
+              <h2 className="color-selva">
+                {isInclusive && (
+                  <FormattedMessage
+                    id={`storyFactory.subwelcome_inc`}
+                    defaultMessage="A place for silly syllabic reading!"
+                    description="Sub welcome message on Story Factory"
+                  />
+                )}
+                {!isInclusive && (
+                  <FormattedMessage
+                    id={`storyFactory.subwelcome`}
+                    defaultMessage="A place for silly syllabic reading!"
+                    description="Sub welcome message on Story Factory"
+                  />
+                )}
+              </h2>
+
+              {!isImmersive && (
+                <>
+                  <h1>
+                    <br />
+                    Welcome to the story factory!
+                  </h1>
+                  <h2>A place for silly syllabic reading!</h2>
+                </>
+              )}
+            </div>
+          </IonCardContent>
+        </IonCard>
+      </div>
+      <img className="bili-character" src={image} alt="Bili character" />
+    </div>
+  );
+};
