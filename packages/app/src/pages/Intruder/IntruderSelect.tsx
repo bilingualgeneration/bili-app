da6eb53764ae9f02a82047e8efe5b638ef1f1523
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-02-22 16:40:12 -0600

    intruder pack select done

--- packages/app/src/pages/Intruder/IntruderSelect.tsx
+++ packages/app/src/pages/Intruder/IntruderSelect.tsx
@@ -1,26 +1,28 @@
 import { PackSelect } from "@/components/PackSelect";
+import boot from '../../../public/assets/img/boot_image.png';
+import horse from '../../../public/assets/img/horse_image.png';
 
 export const IntruderSelect: React.FC = () => {
   const placeholderCards = [
     {
-      title: 'abc',
-      titleEn: 'def',
+      title: 'Paquete 2',
+      titleEn: 'Pack 2',
       category: 'play',
-      cover: '',
+      cover: boot,
       isLocked: true
     },
     {
-      title: '',
-      titleEn: '',
-      category: '',
-      cover: '',
+      title: 'Paquete 3',
+      titleEn: 'Pack 3',
+      category: 'play',
+      cover: horse,
       isLocked: true
     },
     {
-      title: '',
-      titleEn: '',
-      category: '',
-      cover: '',
+      title: 'Paquete 4',
+      titleEn: 'Pack 4',
+      category: 'play',
+      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/stories_friends_cover_e66b64561c.svg',
       isLocked: true
     },
   ];
