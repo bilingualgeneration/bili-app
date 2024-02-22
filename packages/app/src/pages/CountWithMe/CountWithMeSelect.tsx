8f19b337e4236a1495d1d42e301ebeec59f1c838
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-02-22 16:14:15 -0600

    story factory pack select done

--- packages/app/src/pages/CountWithMe/CountWithMeSelect.tsx
+++ packages/app/src/pages/CountWithMe/CountWithMeSelect.tsx
@@ -2,10 +2,12 @@ import { PackSelect } from "@/components/PackSelect";
 
 
 export const CountWithMeSelect: React.FC = () => {
-  return <PackSelect
-           module="count-with-me-game"
-	   category="play"
-           translatedTitle='Cuenta Conmigo'
-           englishTitle="Count With Me"
-  />;
+  return (
+    <PackSelect
+      module="count-with-me-game"
+      category="play"
+      translatedTitle='Cuenta Conmigo'
+      englishTitle="Count With Me"
+    />
+  );
 }
--- packages/app/src/pages/StoryFactory/StoryFactorySelect.tsx
+++ packages/app/src/pages/StoryFactory/StoryFactorySelect.tsx
@@ -3,32 +3,34 @@ import { PackSelect } from "@/components/PackSelect";
 export const StoryFactorySelect: React.FC = () => {
   const placeholderCards = [
     {
-      title: 'abc',
-      titleEn: 'def',
+      title: 'Paquete 2',
+      titleEn: 'Pack 2',
       category: 'play',
-      cover: '',
+      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/stories_friends_cover_e66b64561c.svg',
       isLocked: true
     },
     {
-      title: '',
-      titleEn: '',
-      category: '',
-      cover: '',
+      title: 'Paquete 3',
+      titleEn: 'Pack 3',
+      category: 'play',
+      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/4_cover_El_esqueleto_travieso_e992b9d069.svg',
       isLocked: true
     },
     {
-      title: '',
-      titleEn: '',
-      category: '',
-      cover: '',
+      title: 'Paquete 4',
+      titleEn: 'Pack 4',
+      category: 'play',
+      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/thumbnail_Image_Container3_49e525e22a.png',
       isLocked: true
     },
   ];
-  return <PackSelect
-           module="story-factory-game"
-	   category="play"
-           translatedTitle='¡Fábrica de Cuentos!'
-           englishTitle="Story Factory"
-	   placeholderCards={placeholderCards}
-  />;
+  return (
+    <PackSelect
+      module="story-factory-game"
+      category="play"
+      translatedTitle='¡Fábrica de Cuentos!'
+      englishTitle="Story Factory"
+      placeholderCards={placeholderCards}
+    />
+  );
 }
