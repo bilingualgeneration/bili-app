e013ca43d49978c561ae597840f2394a366f2ec5
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-02-22 16:14:15 -0600

    Revert "story factory pack select done"
    This reverts commit 8f19b337e4236a1495d1d42e301ebeec59f1c838.

--- packages/app/src/pages/CountWithMe/CountWithMeSelect.tsx
+++ packages/app/src/pages/CountWithMe/CountWithMeSelect.tsx
@@ -2,12 +2,10 @@ import { PackSelect } from "@/components/PackSelect";
 
 
 export const CountWithMeSelect: React.FC = () => {
-  return (
-    <PackSelect
-      module="count-with-me-game"
-      category="play"
-      translatedTitle='Cuenta Conmigo'
-      englishTitle="Count With Me"
-    />
-  );
+  return <PackSelect
+           module="count-with-me-game"
+	   category="play"
+           translatedTitle='Cuenta Conmigo'
+           englishTitle="Count With Me"
+  />;
 }
--- packages/app/src/pages/StoryFactory/StoryFactorySelect.tsx
+++ packages/app/src/pages/StoryFactory/StoryFactorySelect.tsx
@@ -3,34 +3,32 @@ import { PackSelect } from "@/components/PackSelect";
 export const StoryFactorySelect: React.FC = () => {
   const placeholderCards = [
     {
-      title: 'Paquete 2',
-      titleEn: 'Pack 2',
+      title: 'abc',
+      titleEn: 'def',
       category: 'play',
-      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/stories_friends_cover_e66b64561c.svg',
+      cover: '',
       isLocked: true
     },
     {
-      title: 'Paquete 3',
-      titleEn: 'Pack 3',
-      category: 'play',
-      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/4_cover_El_esqueleto_travieso_e992b9d069.svg',
+      title: '',
+      titleEn: '',
+      category: '',
+      cover: '',
       isLocked: true
     },
     {
-      title: 'Paquete 4',
-      titleEn: 'Pack 4',
-      category: 'play',
-      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/thumbnail_Image_Container3_49e525e22a.png',
+      title: '',
+      titleEn: '',
+      category: '',
+      cover: '',
       isLocked: true
     },
   ];
-  return (
-    <PackSelect
-      module="story-factory-game"
-      category="play"
-      translatedTitle='¡Fábrica de Cuentos!'
-      englishTitle="Story Factory"
-      placeholderCards={placeholderCards}
-    />
-  );
+  return <PackSelect
+           module="story-factory-game"
+	   category="play"
+           translatedTitle='¡Fábrica de Cuentos!'
+           englishTitle="Story Factory"
+	   placeholderCards={placeholderCards}
+  />;
 }
