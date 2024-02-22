7967b1aacc3d71be9920213ce16eb4e73406fe56
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2024-02-22 16:37:03 -0600

    would do pack select done

--- packages/app/src/pages/WouldDo/WouldDoSelect.tsx
+++ packages/app/src/pages/WouldDo/WouldDoSelect.tsx
@@ -1,26 +1,27 @@
 import { PackSelect } from "@/components/PackSelect";
+import boot from '../../../public/assets/img/boot_image.png';
 
 export const WouldDoSelect: React.FC = () => {
   const placeholderCards = [
     {
-      title: 'abcasddassad',
-      titleEn: 'def',
+      title: 'Nivel 3',
+      titleEn: 'Level 3',
       category: 'community',
-      cover: '',
+      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/stories_friends_cover_e66b64561c.svg',
       isLocked: true
     },
     {
-      title: '',
-      titleEn: '',
-      category: '',
-      cover: '',
+      title: 'Nivel 4',
+      titleEn: 'Level 4',
+      category: 'community',
+      cover: 'https://bili-strapi-media-dev.s3.us-east-1.amazonaws.com/thumbnail_drum_image_42dab560fb.png',
       isLocked: true
     },
     {
-      title: '',
-      titleEn: '',
-      category: '',
-      cover: '',
+      title: 'Nivel 5',
+      titleEn: 'Level 5',
+      category: 'community',
+      cover: boot,
       isLocked: true
     },
   ];
