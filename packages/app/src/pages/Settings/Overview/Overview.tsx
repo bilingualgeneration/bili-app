2ae36d7a295cc819453a949cb352f022e7fda952
Jon Chin <jon@sharebyteaching.org>
2024-02-27 12:00:16 -0500

    minor fix

--- packages/app/src/pages/Settings/Overview/Overview.tsx
+++ packages/app/src/pages/Settings/Overview/Overview.tsx
@@ -43,7 +43,7 @@ export const Overview: React.FC = ({}) => {
           setShouldShowTutorial(true);
           Preferences.set({
             key: "shouldShowSettingsTutorial",
-            value: false,
+            value: "false",
           });
         }
       });
