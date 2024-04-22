89a7713581c3ca912ff9029411db73fb35ce5cba
Jon Chin <jon@sharebyteaching.org>
2024-04-22 12:22:46 -0400

    updated for running on ios

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "com.thebiliapp.app",
   appName: "bili",
   webDir: "dist",
   server: {
