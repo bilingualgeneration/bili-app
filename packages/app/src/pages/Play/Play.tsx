2c7f496b7dffefdd75e8db511cc8f2c6552ee9b1
Bharath Sriraam R R <albharath.1305@gmail.com>
2024-05-15 13:17:18 -0400

    Fix UI bug in Play cards

--- packages/app/src/pages/Play/Play.tsx
+++ packages/app/src/pages/Play/Play.tsx
@@ -1,120 +1,116 @@
 import { FC } from "react";
-import { IonCard, IonCardContent, IonText } from "@ionic/react";
+import { IonText } from "@ionic/react";
 import { useHistory } from "react-router-dom";
-import { useIntl } from "react-intl";
 import CountWithMe from "@/assets/icons/count_with_me.png";
 import MagnifyingGlass from "@/assets/icons/magnifying_glass.png";
 import FactoryWidget from "@/assets/icons/factory_widget.png";
 import { FavoriteButton } from "@/components/FavoriteButton";
 import { FormattedMessage } from "react-intl";
 import { PlayHeader } from "@/components/PlayHeader";
-import { Link } from "react-router-dom";
 import { Carousel } from "@/components/Carousel";
 import { CategoryTag } from "@/components/CategoryTag";
-import {useLanguageToggle} from '@/components/LanguageToggle';
+import { useLanguageToggle } from "@/components/LanguageToggle";
 
 import "./Play.scss";
 
-const StoryFactoryCard: FC = () => {
-  const {language} = useLanguageToggle();
+type PlayCardProps = {
+  id: string;
+  url: string;
+  imgUrl: string;
+  title: {
+    id: string;
+    defaultMessage: string;
+    description: string;
+  };
+  altTitle: string;
+  fid: string;
+};
+
+const PlayCard = ({ id, url, imgUrl, title, altTitle, fid }: PlayCardProps) => {
+  const { language } = useLanguageToggle();
   const history = useHistory();
   return (
     <div
-      id="storyFactoryCard"
+      id={id}
       className="card"
       onClick={() => {
-        history.push("/story-factory-game/intro");
+        history.push(url);
       }}
     >
       <CategoryTag category="play" className="play-category-tag" />
-      <FavoriteButton fid="category-story factory" />
-      <img src={FactoryWidget} />
-      <IonText>
-        <h1 className="text-4xl semibold">
-          <FormattedMessage
-            id="common.storyFactory"
-            defaultMessage="Story Factory!"
-            description="Standalone label for Story Factory"
-          />
-        </h1>
-        {language === 'esen' && <p className="text-3xl color-nube">Story Factory</p>}
-      </IonText>
+      <img src={imgUrl} />
+
+      <div className="cardTitles">
+        <IonText>
+          <h1 className="text-4xl semibold">
+            <FormattedMessage
+              id={title.id}
+              defaultMessage={title.defaultMessage}
+              description={title.description}
+            />
+          </h1>
+        </IonText>
+
+        <IonText>
+          {language === "esen" && (
+            <p className="text-3xl color-nube">{altTitle}</p>
+          )}
+        </IonText>
+      </div>
+
+      <FavoriteButton fid={fid} />
     </div>
   );
 };
 
-const IntruderCard: FC = () => {
-  const {language} = useLanguageToggle();
-  const history = useHistory();
-  return (
-    <div
-      id="intruderCard"
-      className="card"
-      onClick={() => {
-        history.push("/intruder-game/intro");
-      }}
-    >
-      <CategoryTag category="play" className="play-category-tag" />
-      <FavoriteButton fid="category-the intruder" />
-      <img src={MagnifyingGlass} />
-      <IonText>
-        <h1 className="text-4xl semibold">
-          <FormattedMessage
-            id="common.theIntruder"
-            defaultMessage="The Intruder"
-            description="Standalone label for The Intruder"
-          />
-        </h1>
-
-        {language === 'esen' && <p className="text-3xl color-nube">The Intruder</p>}
-      </IonText>
-    </div>
-  );
-};
-
-const CountCard: FC = () => {
-  const {language} = useLanguageToggle();
-  const history = useHistory();
-  return (
-    <div
-      id="countCard"
-      className="card"
-      onClick={() => {
-        history.push(
-          "/count-with-me-game/intro",
-        );
-      }}
-    >
-      <CategoryTag category="play" className="play-category-tag" />
-      <FavoriteButton fid="category-count with me" />
-      <img src={CountWithMe} />
-      <IonText>
-        <h1 className="text-4xl semibold">
-          <FormattedMessage
-            id="common.countWithMe"
-            defaultMessage="Count with Me"
-            description="Standalone label for Count with Me"
-          />
-        </h1>
-        {language === 'esen' && <p className="text-3xl color-nube">Count with Me</p>}
-      </IonText>
-    </div>
-  );
-};
+const playCardData: PlayCardProps[] = [
+  {
+    id: "storyFactoryCard",
+    url: "/story-factory-game/intro",
+    imgUrl: FactoryWidget,
+    title: {
+      id: "common.storyFactory",
+      defaultMessage: "Story Factory!",
+      description: "Standalone label for Story Factory",
+    },
+    altTitle: "Story Factory",
+    fid: "category-story factory",
+  },
+  {
+    id: "intruderCard",
+    url: "/intruder-game/intro",
+    imgUrl: MagnifyingGlass,
+    title: {
+      id: "common.theIntruder",
+      defaultMessage: "The Intruder",
+      description: "Standalone label for The Intruder",
+    },
+    altTitle: "The Intruder",
+    fid: "category-the intruder",
+  },
+  {
+    id: "countCard",
+    url: "/count-with-me-game/intro",
+    imgUrl: CountWithMe,
+    title: {
+      id: "common.countWithMe",
+      defaultMessage: "Count with Me",
+      description: "Standalone label for Count with Me",
+    },
+    altTitle: "Count with Me",
+    fid: "category-count with me",
+  },
+];
 
 export const Play: FC = () => {
-  const intl = useIntl();
   return (
     <div id="playPage">
       <PlayHeader />
       <div className="carousel-container">
-        <Carousel 
-          height={445}
-          slideMargin={10}
-        >
-          <StoryFactoryCard />
-          <IntruderCard />
-          <CountCard />
+        <Carousel height={445} slideMargin={10}>
+          {playCardData.map((card) => (
+            <PlayCard key={card.id} {...card} />
+          ))}
         </Carousel>
       </div>
     </div>
