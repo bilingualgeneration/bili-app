93dc8c458127be809dd83870ea867d79e865c83d
Jon Chin <jon@sharebyteaching.org>
2024-07-11 07:25:57 -0400

    added version number

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,11 +1,15 @@
 import legacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
+import packageJson from './package.json';
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version)
+  },
   plugins: [legacy(), react(), svgr(), tsconfigPaths()],
   test: {
     globals: true,
