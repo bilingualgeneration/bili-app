19eb49973a98e7589537434528926d76849ca5e0
Jon Chin <jon@sharebyteaching.org>
2024-10-28 03:50:17 -0400

    initial testing of sending emails programmatically

--- packages/server/functions/src/index.ts
+++ packages/server/functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as caregiver from "./caregiver";
 import * as classroom from "./classroom";
+export * from "./debug";
 import * as teacher from "./teacher";
 import * as student from "./student";
 import * as user from "./user";
