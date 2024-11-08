4d538dd05a1f7dd9c40de054ad8dbf4d59fd7a0b
ariannaluis <ariannaluis@gmail.com>
2024-11-08 18:46:10 -0500

    resize back button for mobile

--- packages/app/src/components/BackButton/BackButton.tsx
+++ packages/app/src/components/BackButton/BackButton.tsx
@@ -1,18 +1,16 @@
 import backButton from "@/assets/icons/back_button_arrow.svg";
 import { IonButton, IonIcon } from "@ionic/react";
 import { useHistory } from "react-router-dom";
+import "./BackButton.scss";
 
 export const BackButton: React.FC = () => {
   const history = useHistory();
   return (
-    <>
-      <IonButton
-        className="flamenco-high curved-corners"
-        size="large"
-        onClick={history.goBack}
-      >
-        <IonIcon slot="icon-only" icon={backButton} />
-      </IonButton>
-    </>
+    <IonButton
+      className="flamenco-high curved-corners custom-back-button"
+      onClick={history.goBack}
+    >
+      <IonIcon slot="icon-only" icon={backButton} />
+    </IonButton>
   );
 };
