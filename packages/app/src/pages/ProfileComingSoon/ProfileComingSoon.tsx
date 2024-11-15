2390754fb19dfbf4ede43addac77d111ec868ab0
Aleksandra Makhrova <amakhro@gmail.com>
2024-11-15 11:04:50 -0800

    Lnaguage iupdate for ProfileComingSoon

--- packages/app/src/pages/ProfileComingSoon/ProfileComingSoon.tsx
+++ packages/app/src/pages/ProfileComingSoon/ProfileComingSoon.tsx
@@ -8,7 +8,8 @@ import { Carousel } from "@/components/Carousel";
 import "./ProfileComingSoon.scss";
 import biliHelmet from "@/assets/icons/profile_coming_soon/bili_profile_coming_soon.svg";
 import biliWorkshop from "@/assets/icons/profile_coming_soon/bili_workshop.svg";
-import { useLanguageToggle } from '@/components/LanguageToggle';
+import { useLanguageToggle } from "@/components/LanguageToggle";
+import { useI18n } from "@/hooks/I18n";
 
 export const ProfileComingSoon: FC = () => {
   const { language } = useLanguageToggle();
@@ -30,16 +31,14 @@ export const ProfileComingSoon: FC = () => {
       title: "Sube de nivel tu perfil a medida que aprendes",
       subtitle: "Level up your profile as you learn",
       cardImage: bili,
-    }
+    },
   ];
-
 
   return (
     <>
       <PackHeader
         bannerColor="#FFF8F0"
-        title={language === 'en' ? 'My Profile - Coming Soon!' : 'Mi Perfil - Próximamente'}
-        subtitle="My Profile - Coming Soon!"
+        id="studentProfile.comingSoon.title"
         titleClassName="text-5xl color-suelo"
         subtitleClassName="text-3xl color-english semibold"
       />
@@ -47,36 +46,29 @@ export const ProfileComingSoon: FC = () => {
         <div className="content-container">
           <div className="heading-container">
             <h1 className="text-6xl bold carousel-header-margin">
-              {language !== 'en' && 'Nuevas funciones en camino'}
-              {language === 'en' && 'New features on the way'}
+              {language !== "en" && "Nuevas funciones en camino"}
+              {language === "en" && "New features on the way"}
             </h1>
-            {language === 'esen' &&
-              <p className="text-4xl color-english">
-                New features on the way
-              </p>
-            }
+            {language === "esen" && (
+              <p className="text-4xl color-english">New features on the way</p>
+            )}
           </div>
 
-          <img src={biliHelmet} alt="Bili Character Icon" className="bili-helmet-image" />
+          <img
+            src={biliHelmet}
+            alt="Bili Character Icon"
+            className="bili-helmet-image"
+          />
 
           <div className="margin-top-3">
-            <Carousel
-              height={240}
-              slideMargin={15}
-              infinite={true}
-            >
+            <Carousel height={240} slideMargin={15} infinite={true}>
               {profileCards.map((card, index) => (
-                <ComingSoonCard
-                  {...card}
-                  key={index}
-                />
+                <ComingSoonCard {...card} key={index} />
               ))}
             </Carousel>
           </div>
         </div>
-
       </div>
-
     </>
   );
 };
