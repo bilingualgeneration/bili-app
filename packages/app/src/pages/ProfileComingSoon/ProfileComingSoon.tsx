e6caab95abb720aeb1ef7a5a606ee03cf159335a
Jon Chin <jon@sharebyteaching.org>
2024-12-15 16:39:20 -0500

    updated

--- packages/app/src/pages/ProfileComingSoon/ProfileComingSoon.tsx
+++ packages/app/src/pages/ProfileComingSoon/ProfileComingSoon.tsx
@@ -5,8 +5,6 @@ import stars from "@/assets/icons/profile_coming_soon/coming_soon_card_stars.svg
 import stickers from "@/assets/icons/profile_coming_soon/coming_soon_card_stickers.svg";
 import bili from "@/assets/icons/profile_coming_soon/coming_soon_card_bili.svg";
 import { Carousel } from "@/components/Carousel";
-import "./ProfileComingSoon.scss";
-import biliHelmet from "@/assets/icons/profile_coming_soon/bili_profile_coming_soon.svg";
 import biliWorkshop from "@/assets/icons/profile_coming_soon/bili_workshop.svg";
 import { useLanguageToggle } from "@/components/LanguageToggle";
 import { useI18n } from "@/hooks/I18n";
@@ -17,55 +15,78 @@ export const ProfileComingSoon: FC = () => {
   const profileCards = [
     {
       cardColor: "#9A90F0",
-      title: "Gana estrellas para desbloquear logros.",
-      subtitle: "Earn stars to unlock achievements",
+      text: [
+        {
+          text: "Gana estrellas para desbloquear logros.",
+          language: "es",
+        },
+        {
+          text: "Earn stars to unlock achievements",
+          language: "en",
+        },
+      ],
       cardImage: stars,
     },
     {
       cardColor: "#FF5708",
-      title: "Colecciona calcomanías mientras juegas",
-      subtitle: "Collect stickers as you play",
+      text: [
+        {
+          text: "Colecciona calcomanías mientras juegas",
+          language: "es",
+        },
+        {
+          text: "Collect stickers as you play",
+          language: "en",
+        },
+      ],
       cardImage: stickers,
     },
     {
       cardColor: "#EC59B1",
-      title: "Sube de nivel tu perfil a medida que aprendes",
-      subtitle: "Level up your profile as you learn",
+      text: [
+        {
+          text: "Sube de nivel tu perfil a medida que aprendes",
+          language: "es",
+        },
+        {
+          text: "Level up your profile as you learn",
+          language: "en",
+        },
+      ],
       cardImage: bili,
     },
   ];
 
   return (
     <>
-      <PackHeader bannerColor="#FFF8F0" id="studentProfile.comingSoon.title" />
-      <div id="personal-student-profile">
-        <div className="content-container">
-          <div className="heading-container">
-            <h1 className="text-6xl bold carousel-header-margin">
-              <I18nMessage id={"studentProfile.comingSoon.features"} />
-            </h1>
-            <I18nMessage
-              id={"studentProfile.comingSoon.features"}
-              level={2}
-              wrapper={(text: string) => (
-                <p className="text-4xl color-english">{text}</p>
-              )}
-            />
-          </div>
-
-          <img
-            src={biliHelmet}
-            alt="Bili Character Icon"
-            className="bili-helmet-image"
-          />
-
-          <div className="margin-top-3">
-            <Carousel height={240} slideMargin={15} infinite={true}>
-              {profileCards.map((card, index) => (
-                <ComingSoonCard {...card} key={index} />
-              ))}
-            </Carousel>
-          </div>
+      <PackHeader
+        bannerColor="#FFF8F0"
+        id="studentProfile.comingSoon.title"
+        className="color-suelo"
+      />
+      <div
+        className="padding-horizontal-3"
+        style={{
+          background: `url(${biliWorkshop}) no-repeat top 0 right 2rem`,
+          backgroundSize: "contain",
+        }}
+      >
+        <h1 className="text-6xl bold">
+          <I18nMessage id={"studentProfile.comingSoon.features"} />
+        </h1>
+        <I18nMessage
+          id={"studentProfile.comingSoon.features"}
+          level={2}
+          wrapper={(text: string) => (
+            <p className="text-4xl color-english">{text}</p>
+          )}
+        />
+        <div className="margin-top-3">
+          <Carousel height={240} slideMargin={15} infinite={true}>
+            {profileCards.map((card, index) => (
+              <ComingSoonCard {...card} key={index} />
+            ))}
+          </Carousel>
         </div>
       </div>
     </>
