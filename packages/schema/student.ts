4551b29cdff551f92177c39605e2eefdf965ad35
Jon Chin <jon@sharebyteaching.org>
2024-12-16 08:31:51 -0500

    made some fields optional

--- packages/schema/student.ts
+++ packages/schema/student.ts
@@ -4,5 +4,5 @@ export const studentSchema = z.object({
   firstName: z.string(),
   lastName: z.string().optional(),
   classroom: z.array(z.string()),
-  caregiverEmail: z.array(z.string().email()),
+  caregiverEmail: z.array(z.string().email()).optional(),
 });
