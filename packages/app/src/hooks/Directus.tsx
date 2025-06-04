df979fc6b8efde8fec1d78a54b33b6d811c78d78
Jon Chin <jon@sharemeals.org>
2025-06-04 13:14:56 -0400

    classlink sso wip

--- packages/app/src/hooks/Directus.tsx
+++ packages/app/src/hooks/Directus.tsx
@@ -1,7 +1,13 @@
-import { createDirectus, rest } from "@directus/sdk";
+import { authentication, createDirectus, rest } from "@directus/sdk";
 
 import type { ApiCollections } from "@/types/directus.d.ts";
 
 export const directus = createDirectus<ApiCollections>(
   import.meta.env.VITE_DIRECTUS_URL,
-).with(rest());
+)
+  .with(authentication("session", { credentials: "include" }))
+  .with(
+    rest({
+      credentials: "include",
+    }),
+  );
